bf28f260c92fe2502741a335708233fceddbc41f
yixinzhang12 <yixinzhang111@gmail.com>
2024-08-29 15:33:54 -0700

    Added auto parameter calculating function

--- code/main_analysis.R
+++ code/main_analysis.R
@@ -14,7 +14,7 @@ process_all_trials <- function(folder_name) {
   raw_files <- list.files(data_folder, pattern = "Trial\\d+\\.txt$", full.names = TRUE)
   
   for (file in raw_files) {
-    read_and_process_file(file)
+    clean_file(file)
   }
   
   cleaned_files <- list.files(data_folder, pattern = "Trial\\d+_cleaned\\.csv$", full.names = TRUE)
@@ -25,5 +25,5 @@ process_all_trials <- function(folder_name) {
 }
 
 # Example usage
-folder_name <- "Izzy_Pilot"
+folder_name <- "Calvin_Pilot"
 process_all_trials(folder_name)
